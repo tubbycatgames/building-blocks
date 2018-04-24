6a3e03c41656ef93724e0d8a70b545de456f316f
Phil Simmons <phil@tubbycatgames.com>
2018-04-24 08:05:11 -0400

    Added basic assets and level

--- src/building_blocks/core.cljs
+++ src/building_blocks/core.cljs
@@ -1,7 +1,28 @@
 (ns building-blocks.core
   (:require [phzr.core :as p]
-            [phzr.game :as pg]))
+            [phzr.game :as pg]
+            [phzr.game-object-factory :as pgof]
+            [phzr.loader :as pl]))
 
 (enable-console-print!)
 
-(pg/->Game 800 600 (p/phaser-constants :auto) "app")
+(defn ^:private preload
+  [game]
+  (let [loader (:load game)]
+    (doto loader
+      (pl/image "brick" "assets/brick.png")
+      (pl/image "grass" "assets/grass.png")
+      (pl/image "sky" "assets/sky.png"))))
+
+(defn ^:private create
+  [game]
+  (let [gof (:add game)
+        height (-> game :world :height)
+        width (-> game :world :width)]
+    (pgof/tile-sprite gof 0 0 width height "sky")
+    (pgof/sprite gof 0 (- height 80) "brick")
+    (pgof/tile-sprite gof 0 (- height 50) width 50 "grass")))
+
+(pg/->Game 800 600 (p/phaser-constants :auto) "app"
+  {"preload" preload
+   "create" create})
